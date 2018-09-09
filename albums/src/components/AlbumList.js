5c955b094c964fd6a6d86155d44418c8c6d91d4f
Tran Quoc Bao <bao.tq@sutrixsolutions.com>
2018-09-09 23:52:59 +0700

    Created a list of albums to show each album details to the user

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,13 +1,43 @@
 // Import libraries for making a component
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>AlbumList!!!</Text>
-    </View>
-  );
-};
+// Replace this functional component with a class based component
+// const AlbumList = () => {
+//   return (
+//     <View>
+//       <Text>AlbumList!!!</Text>
+//     </View>
+//   );
+// };
+
+class AlbumList extends Component {
+  state = { albums: [] };
+
+  componentWillMount() {
+    // console.log('componentWillMount in AlbumList');
+    // debugger;
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+         .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album => 
+      <AlbumDetail key={album.title} album={album} />
+    );
+  } 
+
+  render() {
+    console.log(this.state);
+
+    return (
+      <View>
+        { this.renderAlbums() }
+      </View>
+    );
+  }
+}
 
 export default AlbumList;
